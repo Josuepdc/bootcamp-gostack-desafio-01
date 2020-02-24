894f896c594509fe8adb6652caeb3279bc36a043
Josué Pereira da Conceição <josue.conceicao@radixeng.com.br>
2020-02-23 22:55:09 -0300

    Implementação do recurso de projetos com listagem geral, criação, edição, deleção e criação de tarefas com armazenamento mockado em memória

--- index.js
+++ index.js
@@ -5,7 +5,60 @@ const server = express();
 // Necessário para receber json por post
 server.use(express.json());
 
+// Mock de armazenamento de dados de projetos em memória
+const projectsData = new Map();
+Map.prototype.toArray = function() {
+    return [...this.values()];
+};
 
+// Listar todos os projetos
+server.get("/projects", (req, res) => {
+    return res.json(projectsData.toArray());
+});
 
+// Criar projeto
+server.post("/projects", (req, res) => {
+    const { id, title } = req.body;
+
+    projectsData.set(id, {id, title});
+
+    return res.json(projectsData.toArray());
+});
+
+// Editar projeto
+server.put("/projects/:id", (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    const projectData = projectsData.get(id);
+    projectData.title = title;
+    projectsData.set(id, projectData);
+
+    return res.json(projectsData.toArray());
+});
+
+// Deletar projeto
+server.delete("/projects/:id", (req, res) => {
+    const { id } = req.params;
+
+    projectsData.delete(id);
+
+    return res.json(projectsData.toArray());
+});
+
+// Deletar projeto
+server.post("/projects/:id/tasks", (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    const projectData = projectsData.get(id);
+    if(!projectData.tasks) {
+        projectData.tasks = [];        
+    }
+    projectData.tasks.push(title);
+    projectsData.set(id, projectData);
+
+    return res.json(projectsData.toArray());
+});
 
 server.listen(3000);
